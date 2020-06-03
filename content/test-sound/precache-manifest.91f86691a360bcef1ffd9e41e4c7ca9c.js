6b1387bfe4e67261a50c32faf29d80c7f187d2d8
Muhammad Hanif <hanifeij@yahoo.co.id>
2020-06-03 11:21:50 +0700

    add test-sound react example

---
+++ content/test-sound/precache-manifest.91f86691a360bcef1ffd9e41e4c7ca9c.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c5a39d9695c6d0c5d93ee9e580b08e41",
+    "url": "/test-sound/index.html"
+  },
+  {
+    "revision": "48f503fbc89e5888c881",
+    "url": "/test-sound/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "18e82138a9c95a162625",
+    "url": "/test-sound/static/js/2.7ec8c2bf.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/test-sound/static/js/2.7ec8c2bf.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "48f503fbc89e5888c881",
+    "url": "/test-sound/static/js/main.02c39e07.chunk.js"
+  },
+  {
+    "revision": "99638cc7cf4cc92d4411",
+    "url": "/test-sound/static/js/runtime-main.6231c1ae.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/test-sound/static/media/logo.5d5d9eef.svg"
+  }
+]);
