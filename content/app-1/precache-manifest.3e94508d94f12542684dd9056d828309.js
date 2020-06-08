e1eb1f6b68f862caca716f7a47e4296a85d6a9ac
Muhammad Hanif <hanifeij@yahoo.co.id>
2020-06-08 13:22:53 +0700

    add sample fe-routing app

---
+++ content/app-1/precache-manifest.3e94508d94f12542684dd9056d828309.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e50b6e54155062c5432e4aee571a2394",
+    "url": "/app-1/index.html"
+  },
+  {
+    "revision": "7a990562cc98bbafc4a4",
+    "url": "/app-1/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "9b2ed78076c29518d72e",
+    "url": "/app-1/static/js/2.8f79e5e6.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/app-1/static/js/2.8f79e5e6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7a990562cc98bbafc4a4",
+    "url": "/app-1/static/js/main.efe4397c.chunk.js"
+  },
+  {
+    "revision": "eb2d73175aeb2156e7c6",
+    "url": "/app-1/static/js/runtime-main.29216a78.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/app-1/static/media/logo.5d5d9eef.svg"
+  }
+]);
